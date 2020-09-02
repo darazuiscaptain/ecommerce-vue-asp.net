9ba629194d27edc7888a417c0dd11bd899264d49
darazuiscaptain <smallcaptain@proton.me>
2020-09-02 12:57:24 +0800

    Styling the product list and product details components

--- Ecommerce/clientapp/src/components/catalogue/ProductList.vue
+++ Ecommerce/clientapp/src/components/catalogue/ProductList.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="products">
-    <h1>Products</h1>
-    <div class="list">
-      <div class="item" v-for="product in products" :key="product.slug">
-        <h3 @click="select(product)">{{ product.name }}</h3>
-        <img
-          :src="product.thumbnail"
-          :alt="product.name"
-          @click="select(product)"
-        />
-        <p @click="select(product)">{{ product.shortDescription }}</p>
-        <p @click="select(product)">{{ product.price }}</p>
-        <hr />
-      </div>
-    </div>
+    <b-container>
+      <h1 class="mt-4 mb-4">Products</h1>
+      <b-row>
+        <b-col class="mb-4" sm="6" v-for="product in products" :key="product.id">
+          <b-media class="product">
+            <img slot="aside" :src="product.thumbnail" 
+             :alt="product.name" @click="view(product)"/>
+            <h2 class="mt-2" @click="view(product)">
+              {{product.name}}
+            </h2>
+            <p class="mt-4 mb-4">
+              {{product.shortDescription}}
+            </p>
+            <p class="mt-4 mb-4">{{product.price}}</p>
+            <b-button variant="primary">Add to cart</b-button>
+          </b-media>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -27,38 +32,20 @@ export default {
     },
   },
   methods: {
-    select(product) {
+    view(product) {
       this.$router.push(`/products/${product.slug}`);
     },
   },
 };
 </script>
 
-<style>
-* {
-  box-sizing: border-box !important;
-}
+<style lang="scss" scoped>
 .products {
-  padding: 1.25rem;
-  max-width: 75rem;
-  margin: 0 auto;
+  border: 3px solid #eee;
 }
-/* .list,
-.details {
-  width: 50%;
-  float: left;
-} */
-.list .item {
-  width: 25%;
-  float: left;
-  padding: 1.25rem 0.625rem 1.25rem 0;
-}
-.list img,
-.list h3,
-.list p {
+img,
+h2 {
   cursor: pointer;
 }
-.list img {
-  width: 6.25rem;
-}
+
 </style>
