aba1b9373377837302f87e06f7a1c07e1895513f
darazuiscaptain <smallcaptain@proton.me>
2021-01-08 11:13:58 +0800

    Configuring nested route definitions

--- Ecommerce/clientapp/src/main.js
+++ Ecommerce/clientapp/src/main.js
@@ -39,6 +39,11 @@ import Product from "./pages/Product.vue";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Account from "./pages/Account.vue";
+//import admin pages
+import AdminIndex from "./pages/admin/Index.vue";
+import AdminOrders from "./pages/admin/Orders.vue";
+import AdminProducts from "./pages/admin/Products.vue";
+import AdminCreateProduct from "./pages/admin/CreateProduct.vue";
 
 library.add([
   faChevronLeft,
@@ -83,6 +88,26 @@ const routes = [
     path: "/account",
     component: Account,
     meta: { requiresAuth: true, role: "Customer" },
+  },
+  {
+    path: "/admin",
+    component: AdminIndex,
+    meta: { requiresAuth: true, role: "Admin" },
+    redirect: "/admin/orders",
+    children: [
+      {
+        path: "orders",
+        component: AdminOrders,
+      },
+      {
+        path: "products",
+        component: AdminProducts,
+      },
+      {
+        path: "products/create",
+        component: AdminCreateProduct,
+      },
+    ],
   },
   { path: "*", redirect: "/products" },
 ];
